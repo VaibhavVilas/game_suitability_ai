4f2c86f8dd0c5327aad217dda5ab9ba466c9c29c
Vaibhav <Vaibhav@Beg.com>
2026-09-14 19:12:59 +0530

    Updated cors

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    # allow_origins=[""],
+    allow_origins=["https://game-suitability-ai.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
